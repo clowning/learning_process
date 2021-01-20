e349a6281dc829047e808d52a5002885e76e9e8b
clowning <1073128985@qq.com>
2021-01-20 16:06:08 +0800

    test commit

--- src/main/java/day1/demo01.java
+++ src/main/java/day1/demo01.java
@@ -1,4 +1,45 @@
 package day1;
-
+/*
+题目:
+创建4个学生对象,添加到集合,并遍历
+ */
 public class demo01 {
+    public static void main(String[] args) {
+
+    }
+}
+class student{
+    String name;
+    int age;
+    boolean Ismmale;
+
+    public student(String name, int age, boolean ismmale) {
+        this.name = name;
+        this.age = age;
+        Ismmale = ismmale;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isIsmmale() {
+        return Ismmale;
+    }
+
+    public void setIsmmale(boolean ismmale) {
+        Ismmale = ismmale;
+    }
 }
